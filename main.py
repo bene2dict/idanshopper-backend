30ca062d0f9c7078da5b655d4d46aeaadb205dbc
bene2dict <jeffmainonline@gmail.com>
2025-06-21 13:43:51 +0100

    cors error

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Allow frontend (localhost for development)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= "http://localhost:3000",
+    allow_origins= "https://idanshopper-frontend.onrender.com/",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
